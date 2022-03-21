b4ff91b80776f25921d7bf19a78596d422b5dff1
John <belousion@yahoo.com>
2022-03-21 12:30:01 +0200

    initial commit

---
+++ TODO-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TODO-app
+//
+//  Created by John White Mustache on 21.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
